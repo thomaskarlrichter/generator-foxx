b03243ee3a9ac0a5ae277e3437ce2eff4f22f2f2
Thomas Richter <thomas.karl.richter@gmail.com>
2013-07-26 18:36:19 +0200

    fix

--- app/templates/app.js
+++ app/templates/app.js
@@ -4,7 +4,7 @@ var FoxxApplication = require("org/arangodb/foxx").Application;
 
 var app = new FoxxApplication(applicationContext);
 
-app.get("/wiese", function(req, res) {
+app.get("/", function(req, res) {
     res.set("Content-Type", "text/plain");
       res.body = "Worked!"
 });
