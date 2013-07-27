4f5d1c8078018a96baf6ee863814291a26f8c38f
thomaskarlrichter <thomas.karl.richter@gmail.com>
2013-07-27 16:34:44 +0200

    questions added

--- app/index.js
+++ app/index.js
@@ -24,12 +24,24 @@ FoxxGenerator.prototype.askFor = function askFor() {
   console.log('Welcome to Foxx Generator'.red + ' - ' + 'ladies and gentlemen!'.cyan);
 
   var prompts = [{
-    name: 'foxxName',
-    message: 'What do you whant to call your foxx app?'
+        type: 'confirm',
+        name: 'includeBootstrap',
+        message: 'Would you like to include Twitter Bootstrap?',
+        default: false
+  }, {
+        type: 'confirm',
+        name: 'includeBackboneJS',
+        message: 'Would you like to include Backbonejs?',
+        default: false
+  },{
+        name: 'foxxName',
+        message: 'What do you whant to call your foxx app?'
   }];
 
   this.prompt(prompts, function (props) {
     this.foxxName = props.foxxName;
+    this.includeBootstrap = props.includeBootstrap;
+    this.includeBackboneJS = props.includeBackboneJS;
 
     cb();
   }.bind(this));
@@ -39,6 +51,9 @@ FoxxGenerator.prototype.app = function app() {
   this.mkdir('scripts');
   this.mkdir('files');
   this.mkdir('assets/javascript');
+  this.mkdir('assets/css');
+  if(this.includeBootstrap !== undefined && this.includeBootstrap) this.copy('bootstrap.css','assets/css/bootstrap.css');
+  if(this.includeBackboneJS !== undefined && this.includeBackboneJS) this.copy('backbone.js','assets/javascript/backbone.js');
 
   this.copy('app.js', 'app.js');
   this.copy('setup.js', 'scripts/setup.js');
