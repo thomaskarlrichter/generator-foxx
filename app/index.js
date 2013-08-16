bad688fab00880d5e12180deab87d8551270354d
thomaskarlrichter <thomas.karl.richter@gmail.com>
2013-08-16 10:06:52 +0200

    minor fix

--- app/index.js
+++ app/index.js
@@ -5,99 +5,109 @@ var yeoman = require('yeoman-generator');
 
 
 var FoxxGenerator = module.exports = function FoxxGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+    yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
+    this.on('end', function () {
+        this.installDependencies({ skipInstall: options['skip-install'] });
+    });
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 util.inherits(FoxxGenerator, yeoman.generators.Base);
 
 FoxxGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+    var cb = this.async();
 
-  // have Foxx greet the user.
-  console.log('\n' + this.readFileAsString(path.join(__dirname, 'foxx.logo')).green);
-  console.log('Welcome to Foxx Generator'.red + ' - ' + 'ladies and gentlemen!'.cyan);
+    // have Foxx greet the user.
+    console.log('\n' + this.readFileAsString(path.join(__dirname, 'foxx.logo')).green);
+    console.log('Welcome to Foxx Generator'.red + ' - ' + 'ladies and gentlemen!'.cyan);
 
-  var prompts = [ {
-    name: 'foxxName',
-    message: 'What do you whant to call your foxx app?',
-    default: path.basename(process.cwd())
-  }, {
-    name: 'description',
-    message: 'Description',
-    default: 'The best foxx app ever.'
-  }, {
-    name: 'homepage',
-    message: 'Homepage'
-  }, {
-    name: 'license',
-    message: 'License',
-    default: 'MIT'
-  }, {
-    name: 'githubUsername',
-    message: 'GitHub username'
-  }, {
-    name: 'authorName',
-    message: 'Author\'s Name'
-  }, {
-    name: 'authorEmail',
-    message: 'Author\'s Email'
-  }, {
-    name: 'authorUrl',
-    message: 'Author\'s Homepage'
-  }, {
-    type: 'confirm',
-    name: 'includeBootstrap',
-    message: 'Would you like to include Twitter Bootstrap?',
-    default: false
-  }, {
-    type: 'confirm',
-    name: 'includeBackboneJS',
-    message: 'Would you like to include Backbonejs?',
-    default: false
-  }];
+    var prompts = [
+        {
+            name: 'foxxName',
+            message: 'What do you whant to call your foxx app?',
+            default: path.basename(process.cwd())
+        },
+        {
+            name: 'description',
+            message: 'Description',
+            default: 'The best foxx app ever.'
+        },
+        {
+            name: 'homepage',
+            message: 'Homepage'
+        },
+        {
+            name: 'license',
+            message: 'License',
+            default: 'MIT'
+        },
+        {
+            name: 'githubUsername',
+            message: 'GitHub username'
+        },
+        {
+            name: 'authorName',
+            message: 'Author\'s Name'
+        },
+        {
+            name: 'authorEmail',
+            message: 'Author\'s Email'
+        },
+        {
+            name: 'authorUrl',
+            message: 'Author\'s Homepage'
+        },
+        {
+            type: 'confirm',
+            name: 'includeBootstrap',
+            message: 'Would you like to include Twitter Bootstrap?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'includeBackboneJS',
+            message: 'Would you like to include Backbonejs?',
+            default: false
+        }
+    ];
 
-  this.prompt(prompts, function (props) {
-    this.slugname = this._.slugify(props.foxxName);
+    this.prompt(prompts, function (props) {
+        this.slugname = this._.slugify(props.foxxName);
 
-    this.repoUrl = 'https://github.com/' + props.githubUsername + '/' + this.slugname;
+        this.repoUrl = 'https://github.com/' + props.githubUsername + '/' + this.slugname;
 
-    if (!props.homepage) {
-        props.homepage = this.repoUrl;
-    }
+        if (!props.homepage) {
+            props.homepage = this.repoUrl;
+        }
 
-    this.props = props;
-    this.foxxName = props.foxxName;
-    this.includeBootstrap = props.includeBootstrap;
-    this.includeBackboneJS = props.includeBackboneJS;
+        this.props = props;
+        this.foxxName = props.foxxName;
+        this.description = props.description;
+        this.homepage = props.homepage;
+        this.authorUrl = props.authorUrl;
+        this.authorEmail = props.authorEmail;
+        this.includeBootstrap = props.includeBootstrap;
+        this.includeBackboneJS = props.includeBackboneJS;
 
-    cb();
-  }.bind(this));
+        cb();
+    }.bind(this));
 };
 
 FoxxGenerator.prototype.app = function app() {
-  this.mkdir('scripts');
-  this.mkdir('files');
-  this.mkdir('assets/javascript');
-  this.mkdir('assets/css');
-  this.copy('_bowerrc','.bowerrc');
-  this.copy('_style.css','assets/css/style.css');
-  this.copy('_bower.json','bower.json');
-  this.template('_index.html', 'assets/index.html');
-  //if(this.includeBootstrap) this.copy('bootstrap.css','assets/css/bootstrap.css');
-  //einfügen in index.html link und script tag
-  //if(this.includeBackboneJS) this.copy('backbone.js','assets/javascript/backbone.js');
-  //einfügen in index.html script tag
-
-  this.copy('app.js', 'app.js');
-  this.copy('setup.js', 'scripts/setup.js');
-  this.copy('teardown.js', 'scripts/teardown.js');
-  this.copy('_manifest.json', 'manifest.json');
-  this.copy('_README.md', 'README.md');
-  this.copy('thumbnail.png', 'images/thumbnail.png');
+    this.mkdir('scripts');
+    this.mkdir('files');
+    this.mkdir('assets/javascript');
+    this.mkdir('assets/css');
+    this.copy('_bowerrc', '.bowerrc');
+    this.copy('_style.css', 'assets/css/style.css');
+    this.copy('_bower.json', 'bower.json');
+    this.template('_index.html', 'assets/index.html');
+    this.copy('app.js', 'app.js');
+    this.copy('setup.js', 'scripts/setup.js');
+    this.copy('teardown.js', 'scripts/teardown.js');
+    this.copy('_manifest.json', 'manifest.json');
+    this.copy('_README.md', 'README.md');
+    this.copy('thumbnail.png', 'images/thumbnail.png');
 };
