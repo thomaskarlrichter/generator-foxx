786543d07459f6a49b79ce7110e8ba7a4a7f2aac
thomaskarlrichter <thomas.karl.richter@gmail.com>
2013-08-23 16:06:19 +0200

    minor fix

--- model/templates/model.js
+++ model/templates/model.js
@@ -2,30 +2,14 @@
 /*global require, exports */
 
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief A TODO-List Foxx-Application written for ArangoDB
+/// @brief A
 ///
 /// @file
 ///
 /// DISCLAIMER
 ///
-/// Copyright 2010-2012 triagens GmbH, Cologne, Germany
-///
-/// Licensed under the Apache License, Version 2.0 (the "License");
-/// you may not use this file except in compliance with the License.
-/// You may obtain a copy of the License at
-///
-///     http://www.apache.org/licenses/LICENSE-2.0
-///
-/// Unless required by applicable law or agreed to in writing, software
-/// distributed under the License is distributed on an "AS IS" BASIS,
-/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-/// See the License for the specific language governing permissions and
-/// limitations under the License.
-///
-/// Copyright holder is triAGENS GmbH, Cologne, Germany
-///
-/// @author Michael Hackstein
-/// @author Copyright 2011-2013, triAGENS GmbH, Cologne, Germany
+/// @author
+/// @author
 ////////////////////////////////////////////////////////////////////////////////
 
 (function () {
@@ -34,10 +18,10 @@
     Todo;
 
   // Currently you could use the normal model, but for demo purposes
-  Todo = Foxx.Model.extend({
+  TODO = Foxx.Model.extend({
   });
 
-  exports.Model = Todo;
+  exports.Model = TODO;
 }());
 
 // This is a file copied by your subgenerator.
