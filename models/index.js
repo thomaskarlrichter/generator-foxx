108e49f18159596cb528c869235012ee40ff6507
thomaskarlrichter <thomas.karl.richter@gmail.com>
2013-08-26 14:45:32 +0200

    minor fix

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 'use strict';
 var util = require('util');
+var path = require('path');
 var yeoman = require('yeoman-generator');
+var fileEditUtils = require('../utils.js');
 
 var myname = "";
 var ModelGenerator = module.exports = function ModelGenerator(args, options, config) {
@@ -12,8 +14,42 @@ var ModelGenerator = module.exports = function ModelGenerator(args, options, con
     myname = this.name;
 };
 
+
 util.inherits(ModelGenerator, yeoman.generators.NamedBase);
 
+ModelGenerator.prototype.rewriteAppJs = function () {
+    fileEditUtils.rewriteFile({
+       file: 'app.js',
+       needle: '.todo',
+       splicable: [
+           '// something from ' + myname,
+           '// else'
+       ]
+    });
+};
+
+ModelGenerator.prototype.rewriteSetupJs = function () {
+    fileEditUtils.rewriteFile({
+
+        file: path.join('scripts', 'setup.js'),
+        needle: '->',
+        splicable: [
+            '// something from ' + myname,
+            '// else'
+        ]
+    });
+};
+ModelGenerator.prototype.rewriteTeardownJs = function () {
+    fileEditUtils.rewriteFile({
+
+        file: path.join('scripts', 'teardown.js'),
+        needle: '->',
+        splicable: [
+            '// something from ' + myname,
+            '// else'
+        ]
+    });
+};
 ModelGenerator.prototype.files = function files() {
   this.copy('model.js', 'models/'+ myname + '.js');
 };
